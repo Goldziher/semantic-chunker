5e5dcb65778b00e0a9c64644529e6706a7e0d44b
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2024-07-17 20:46:12 +0200

    chore: removed chunk_content, updated get_chunker and tests

--- semantic_chunker/__init__.py
+++ semantic_chunker/__init__.py
@@ -1,54 +1,117 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Literal, overload
+from json import JSONDecodeError, loads
+from typing import Callable, Literal, Protocol, cast, overload
 
 from semantic_text_splitter import CodeSplitter, MarkdownSplitter, TextSplitter
 from tree_sitter_language_pack import SupportedLanguage, get_binding
-from typing_extensions import TypeAlias
+from typing_extensions import Never
 
-if TYPE_CHECKING:
-    from collections.abc import Generator
+try:
+    from tokenizers import Tokenizer  # type: ignore[import-untyped]
+except ImportError:
+    # We ensure that type checkers will never match this if 'tokenizers' is not installed.
+    Tokenizer = Never
 
-TextualChunkingType = Literal["text", "markdown"]
-CodeChunkingType = Literal["code"]
+_splitters_kw_map: dict[Literal["text", "markdown", "code"], type[TextSplitter | MarkdownSplitter | CodeSplitter]] = {
+    "text": TextSplitter,
+    "markdown": MarkdownSplitter,
+    "code": CodeSplitter,
+}
 
-ChunkingType: TypeAlias = "TextualChunkingType | CodeChunkingType"
+Callback = Callable[[str], int]
+
+
+class SemanticChunker(Protocol):
+    """A protocol representing a semantic chunker."""
+
+    def chunks(self, content: str) -> list[str]:
+        """Generate a list of chunks from a given text. Each chunk will be up to the `capacity`.
+
+        Args:
+            content: The text to chunk.
+
+        Returns:
+            A list of chunks.
+        """
+        ...
+
+    def chunk_with_indices(self, content: str) -> list[tuple[int, str]]:
+        """Generate a list of chunks from a given text, along with their character offsets in the original text. Each chunk will be up to the `capacity`.
+
+        Args:
+            content: The text to chunk.
+
+        Returns:
+            A list of chunks.
+        """
+        ...
+
+
+def _is_valid_json(content: str) -> bool:
+    try:
+        loads(content)
+        return True
+    except JSONDecodeError:
+        return False
+
+
+def _is_json_file_path(content: str) -> bool:
+    return content.endswith(".json")
 
 
 @overload
-def get_chunker(
+def get_chunker(  # type: ignore[no-any-unimported]
+    model: str | Tokenizer | Callback,
     *,
-    chunk_size: int,
-    chunking_type: TextualChunkingType,
-    language: None = None,
-    model: str,
-) -> TextSplitter | MarkdownSplitter: ...
+    chunking_type: Literal["text"],
+    max_tokens: int | tuple[int, int],
+    overlap: int,
+    trim: bool,
+) -> SemanticChunker: ...
 
 
 @overload
-def get_chunker(
+def get_chunker(  # type: ignore[no-any-unimported]
+    model: str | Tokenizer | Callback,
     *,
-    chunk_size: int,
-    chunking_type: CodeChunkingType,
+    chunking_type: Literal["markdown"],
+    max_tokens: int | tuple[int, int],
+    overlap: int,
+    trim: bool,
+) -> SemanticChunker: ...
+
+
+@overload
+def get_chunker(  # type: ignore[no-any-unimported]
+    model: str | Tokenizer | Callback,
+    *,
+    chunking_type: Literal["code"],
     language: SupportedLanguage,
-    model: str,
-) -> CodeSplitter: ...
+    max_tokens: int | tuple[int, int],
+    overlap: int,
+    trim: bool,
+) -> SemanticChunker: ...
 
 
-def get_chunker(
+def get_chunker(  # type: ignore[no-any-unimported]
+    model: str | Tokenizer | Callback,
     *,
-    chunk_size: int,
-    chunking_type: ChunkingType,
+    chunking_type: Literal["text", "markdown", "code"],
     language: SupportedLanguage | None = None,
-    model: str,
-) -> TextSplitter | MarkdownSplitter | CodeSplitter:
+    max_tokens: int | tuple[int, int],
+    overlap: int = 0,
+    trim: bool = True,
+) -> SemanticChunker:
     """Get the chunker for the given chunking type.
 
     Args:
-        chunk_size: The maximal number of tokens per chunk.
+        model: The model name or tokenizer.
         chunking_type: The type of content to chunk.
         language: The coding language to chunk - if the content is code.
-        model: The model name, e.g. gpt-3.5-turbo.
+        max_tokens: The maximal number of tokens per chunk.
+        overlap: The number of allowed to overlap between chunks.
+        trim: Whether to trim whitespace
 
     Raises:
         ValueError: If the language is not provided for code chunking.
@@ -56,62 +119,29 @@ def get_chunker(
     Returns:
         The chunker for the given chunking type.
     """
+    chunker_cls = _splitters_kw_map[chunking_type]
+
+    kwargs = {
+        "capacity": max_tokens,
+        "overlap": overlap,
+        "trim": trim,
+    }
+
     if chunking_type == "code":
-        if not language:
-            raise ValueError("Language is required for code chunking")
+        if language is None:
+            raise ValueError("Language must be provided for code chunking.")
 
-        return CodeSplitter.from_tiktoken_model(
-            model=model,
-            capacity=chunk_size,
-            language=get_binding(language),
-        )
+        kwargs["language"] = get_binding(language)
 
-    if chunking_type == "markdown":
-        return MarkdownSplitter.from_tiktoken_model(model=model, capacity=chunk_size)
+    if isinstance(model, Tokenizer):
+        chunker = chunker_cls.from_huggingface_tokenizer(tokenizer=model, **kwargs)  # type: ignore[arg-type]
+    elif callable(model):
+        chunker = chunker_cls.from_callback(callback=model, **kwargs)  # type: ignore[arg-type]
+    elif _is_valid_json(model):
+        chunker = chunker_cls.from_huggingface_tokenizer_str(json=model, **kwargs)  # type: ignore[arg-type]
+    elif _is_json_file_path(model):
+        chunker = chunker_cls.from_huggingface_tokenizer_file(path=model, **kwargs)  # type: ignore[arg-type]
+    else:
+        chunker = chunker_cls.from_tiktoken_model(model=model, **kwargs)  # type: ignore[arg-type]
 
-    return TextSplitter.from_tiktoken_model(model=model, capacity=chunk_size)
-
-
-@overload
-def chunk_content(
-    content: str,
-    *,
-    chunk_size: int,
-    chunking_type: TextualChunkingType,
-    model: str,
-) -> Generator[str, None, None]: ...
-
-
-@overload
-def chunk_content(
-    content: str,
-    *,
-    chunk_size: int,
-    chunking_type: CodeChunkingType,
-    language: SupportedLanguage,
-    model: str,
-) -> Generator[str, None, None]: ...
-
-
-def chunk_content(
-    content: str,
-    *,
-    chunk_size: int,
-    chunking_type: ChunkingType,
-    language: SupportedLanguage | None = None,
-    model: str,
-) -> Generator[str, None, None]:
-    """Chunk the given content into chunks of the given size.
-
-    Args:
-        content: The content to chunk.
-        chunk_size: The maximal number of tokens per chunk.
-        chunking_type: The type of content to chunk.
-        language: The coding language to chunk - if the content is code.
-        model: The model name, e.g. gpt-3.5-turbo.
-
-    Yields:
-        str: A chunk string
-    """
-    chunker = get_chunker(chunking_type=chunking_type, model=model, chunk_size=chunk_size, language=language)  # type: ignore[arg-type]
-    yield from chunker.chunks(content)
+    return cast(SemanticChunker, chunker)
--- tests/semantic_chunker_test.py
+++ tests/semantic_chunker_test.py
@@ -1,11 +1,11 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Literal
 
 import pytest
 from semantic_text_splitter import CodeSplitter, MarkdownSplitter, TextSplitter
 
-from semantic_chunker import ChunkingType, chunk_content, get_chunker
+from semantic_chunker import get_chunker
 
 if TYPE_CHECKING:
     from tree_sitter_language_pack import SupportedLanguage
@@ -20,19 +20,26 @@ if TYPE_CHECKING:
     ),
 )
 def test_get_chunker(
-    chunking_type: ChunkingType,
+    chunking_type: Literal["text", "markdown", "code"],
     expected_cls: type,
     language: SupportedLanguage | None,
 ) -> None:
     assert isinstance(
-        get_chunker(chunking_type=chunking_type, chunk_size=10, model="gpt-3.5-turbo", language=language),  # type: ignore[arg-type]
+        get_chunker(
+            "gpt-3.5-turbo",
+            chunking_type=chunking_type,  # type: ignore[arg-type]
+            max_tokens=10,
+            overlap=1,
+            trim=False,
+            language=language,  # type: ignore[arg-type]
+        ),
         expected_cls,
     )
 
 
 def test_get_code_chunker_wihtout_language_raises() -> None:
     with pytest.raises(ValueError):
-        get_chunker(chunking_type="code", chunk_size=10, model="gpt-3.5-turbo", language=None)  # type: ignore[call-overload]
+        get_chunker("gpt-3.5-turbo", chunking_type="code", max_tokens=10, language=None)  # type: ignore[call-overload]
 
 
 @pytest.mark.parametrize(
@@ -41,67 +48,63 @@ def test_get_code_chunker_wihtout_language_raises() -> None:
         (
             "text",
             """
-                                    Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin
-                                    literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney
-                                    College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage,
-                                    and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum
-                                    comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by
-                                    Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance.
-                                    The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section
-                                    """,
-            2,
+                                            Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin
+                                            literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney
+                                            College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage,
+                                            and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum
+                                            comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by
+                                            Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance.
+                                            The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section
+                                            """,
+            23,
             None,
         ),
         (
             "markdown",
             """
-                                    # Lorem Ipsum Intro
+                                            # Lorem Ipsum Intro
 
-                                    Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature
-                                    from 45 BC, making it over 2000 years old.
+                                            Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature
+                                            from 45 BC, making it over 2000 years old.
 
-                                    Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin
-                                    words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature,
-                                    discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum"
-                                    (The Extremes of Good and Evil) by Cicero, written in 45 BC.
-                                    This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum,
-                                    "Lorem ipsum dolor sit amet..", comes from a line in section.
-                                    """,
-            2,
+                                            Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin
+                                            words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature,
+                                            discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum"
+                                            (The Extremes of Good and Evil) by Cicero, written in 45 BC.
+                                            This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum,
+                                            "Lorem ipsum dolor sit amet..", comes from a line in section.
+                                            """,
+            24,
             None,
         ),
         (
             "code",
             """
-                             import kotlin.random.Random
+                                     import kotlin.random.Random
 
-                             fun main() {
-                                 val randomNumbers = IntArray(10) { Random.nextInt(1, 100) } // Generate an array of 10 random integers between 1 and 99
-                                 println("Random numbers:")
-                                 for (number in randomNumbers) {
-                                     println(number)  // Print each random number
-                                 }
-                             }
-                             """,
-            2,
+                                     fun main() {
+                                         val randomNumbers = IntArray(10) { Random.nextInt(1, 100) } // Generate an array of 10 random integers between 1 and 99
+                                         println("Random numbers:")
+                                         for (number in randomNumbers) {
+                                             println(number)  // Print each random number
+                                         }
+                                     }
+                                     """,
+            13,
             "kotlin",
         ),
     ),
 )
 def test_chunk_content(
-    chunking_type: ChunkingType,
+    chunking_type: Literal["text", "markdown", "code"],
     content: str,
     expected_chunks: int,
     language: SupportedLanguage | None,
 ) -> None:
-    content = "This is a test string to verify the chunk content functionality over the defined maximum token limit."
-    chunks = list(
-        chunk_content(
-            content=content,
-            model="gpt-3.5-turbo",
-            chunking_type=chunking_type,  # type: ignore[arg-type]
-            chunk_size=10,
-            language=language,  # type: ignore[arg-type]
-        ),
-    )
+    chunks = get_chunker(  # type: ignore[call-overload]
+        "gpt-3.5-turbo",
+        chunking_type=chunking_type,
+        max_tokens=10,
+        language=language,
+    ).chunks(content)
     assert len(chunks) == expected_chunks
